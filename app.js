5de650e67c9d9b88af5fe59ccc946a6ff6888f43
osoriojr <osoriojr@linux.com>
2018-07-30 14:50:12 +0300

    Connecting to node, prepare to distribute

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var fs = require('fs');
 var csv = require('csv-parser')
+var Web3 = require('web3');
 
+var web3 = new Web3();
+var eth = web3.eth;
+
+/* Inicializa conexão com o node local */
+web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+
+web3.eth.defaultAccount = eth.accounts[0];
 
 var inputFilePath = "originalmy-3-years-abc-bounty-program_2018-07-29_03-24-49-winners - Pivot Table 1.csv"
+
+/* TESTNET ABC - Anti Bureaucracy Coin TESTNET */
+var abcAddress = '0xeec0de4a3ebb0233b10f255da9cb8057205744ea';
+var abcAbi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_vanishQty","type":"uint256"}],"name":"vanishToken","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"frozenAccount","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_issueQty","type":"uint256"}],"name":"issueNew","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_target","type":"address"},{"name":"_freeze","type":"bool"}],"name":"freezeAccount","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialAmount","type":"uint256"},{"name":"_tokenName","type":"string"},{"name":"_decimalUnits","type":"uint8"},{"name":"_tokenSymbol","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_target","type":"address"},{"indexed":false,"name":"_frozen","type":"bool"}],"name":"FrozenFunds","type":"event"}];
+var abcContract = web3.eth.contract(abcAbi)
+var abc = abcContract.at(abcAddress)
 
 var stream = csv({
   raw: false,     // do not decode to utf-8 strings
@@ -14,21 +28,27 @@ var stream = csv({
   headers: ['EMAIL', 'WALLET', 'ENTRIES'] // Specifing the headers
 });
 
+var totalDistributed = 0;
+var totalUsers = 0;
+
 fs.createReadStream(inputFilePath)
 .pipe(stream)
 .on('data', function(data){
     try {
-        console.log("Email: "+data.EMAIL);
-        console.log("Wallet is: "+data.WALLET);
-        console.log("Total ABC: "+data.ENTRIES);
-
-        //perform the operation
+        earnedAbc = data.ENTRIES*100000000;
+        console.log("Email: " + data.EMAIL + " Wallet: " + data.WALLET + " ABC: "+ earnedAbc);
+        // abc.transfer(data.WALLET,earnedAbc, {from: web.eth.accounts[0]})
+        totalDistributed += earnedAbc;
+        totalUsers += 1;
     }
     catch(err) {
-        //error handler
+        console.err(err);
     }
 })
 .on('end',function(){
-    console.log('All wallets parsed')
+    console.log('Default account $ETH balance: ' + web3.fromWei(eth.getBalance(web3.eth.defaultAccount)) + ' eth');
+    console.log('ABC Total Supply: '+ abc.totalSupply()/(100000000) + ' ABC');
+    console.log('Total ABC Distributed: ', totalDistributed/100000000 )
+    console.log('Total users: ' + totalUsers )
 });
 
